3c3b81bb0b537295f78667d3aa9733770fcc5955
bydata <ansgar.wolsing@gmail.com>
2022-04-03 18:53:37 +0200

    updated data, changes to plot

--- 2022/05/05-slope-wordle.R
+++ 2022/05/05-slope-wordle.R
@@ -14,42 +14,40 @@ trends <- map(countries, ~gtrends("wordle", geo = .x, gprop = "web", time = time
 trends <- set_names(trends, c("world", countries[2:3]))
 
 df <- map_df(seq_along(trends), ~pluck(trends, .x , "interest_over_time")) %>% 
-  filter(date < as_date("2022-03-17")) %>% 
+  filter(date <= as_date("2022-03-31")) %>% 
   mutate(hits = ifelse(hits == "<1", 0.5, as.numeric(hits))) 
 
 df %>%
   filter(geo == "world") %>% 
   ggplot(aes(date, hits)) +
-  geom_line(size = 1.5, color = "grey12") +
+  geom_line(size = 1.25, color = "grey12") +
   annotate("tile",
            x = seq(as_datetime("2021-10-08"), as_datetime("2022-01-05"), "16 days"),
-           y = 8, width = 14.5 * 86400, height = 10,
+           y = 8.5, width = 14.5 * 86400, height = 10,
            fill = c("#797C7E", "#79A86B", "#797C7E", "#C6B566", "#79A86B", "#797C7E")
   ) +
   annotate("richtext",
            label = c("W", "O", "R", "D", "L", "E"),
            x = seq(as_datetime("2021-10-08"), as_datetime("2022-01-05"), "16 days"),
-           y = 7.5,
+           y = 8,
            hjust = 0.5, vjust = 0.5, size = 7, family = "Helvetica Neue",
            fontface = "bold", color = "white",
            fill = NA, label.size = 0,
-           # fill = c("#797C7E", "#79A86B", "#797C7E", "#C6B566", "#79A86B", "#797C7E"),
            label.padding = unit(0.5, "lines")) +
   annotate("text",
            label = "The Rising Popularity of...",
            x = as_datetime("2021-10-01"),  y = 95, 
-           family = "Helvetica Neue", fontface = "bold", size = 8, hjust = 0) +
+           family = "Helvetica Neue", fontface = "bold", size = 7.5, hjust = 0) +
   annotate("text", x =  as_datetime("2021-10-01"), y = 80, 
            label = str_wrap("The plot shows normalized Google Search hits for \"Wordle\"
            in web searches worldwide from October 2021 to March 2022. A value of 100 denotes 
-           maximum search interest.", width = 58),
+           maximum search interest.", width = 50),
            family = "Noto Serif", size = 3, hjust = 0, color = "grey30") +
+  scale_x_datetime(date_breaks = "1 months", date_labels = "%b") +
   scale_y_continuous(position = "right") +
   labs(
-    caption = "**Source:** Google Trends | **Visualization:** Ansgar Wolsing
-    ",
-    x = NULL, y = "Normalized search interest"
-  ) +
+    caption = "**Source:** Google Trends | **Visualization:** Ansgar Wolsing",
+    x = NULL, y = "Normalized search interest") +
   theme_minimal() +
   theme(
     plot.background = element_rect(color = NA, fill = "white"),
@@ -58,6 +56,5 @@ df %>%
                                           margin = margin(t = 8, b = 4, l = 10)),
     text = element_text(color = "grey30"),
     axis.title.y = element_text(hjust = 0.2),
-    axis.text = element_text(color = "grey50")
-  )
+    axis.text = element_text(color = "grey50"))
 ggsave(here(base_path, "05-slope-wordle.png"), width = 6, height = 5)
