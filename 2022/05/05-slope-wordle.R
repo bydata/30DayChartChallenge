199ce8aeb389129748354f97b59123bec27df8fd
bydata <ansgar.wolsing@gmail.com>
2022-04-03 19:09:23 +0200

    moved "Wordle" text directly under title

--- 2022/05/05-slope-wordle.R
+++ 2022/05/05-slope-wordle.R
@@ -21,27 +21,30 @@ df %>%
   filter(geo == "world") %>% 
   ggplot(aes(date, hits)) +
   geom_line(size = 1.25, color = "grey12") +
+  annotate("text",
+           label = "The Rising Popularity of",
+           x = as_datetime("2021-10-01"),  y = 95, 
+           family = "Helvetica Neue", fontface = "bold", size = 7.5, hjust = 0) +
   annotate("tile",
            x = seq(as_datetime("2021-10-08"), as_datetime("2022-01-05"), "16 days"),
-           y = 8.5, width = 14.5 * 86400, height = 10,
+           y = 84.5, width = 14.5 * 86400, height = 10,
            fill = c("#797C7E", "#79A86B", "#797C7E", "#C6B566", "#79A86B", "#797C7E")
   ) +
   annotate("richtext",
            label = c("W", "O", "R", "D", "L", "E"),
            x = seq(as_datetime("2021-10-08"), as_datetime("2022-01-05"), "16 days"),
-           y = 8,
+           y = 84,
            hjust = 0.5, vjust = 0.5, size = 7, family = "Helvetica Neue",
            fontface = "bold", color = "white",
            fill = NA, label.size = 0,
            label.padding = unit(0.5, "lines")) +
-  annotate("text",
-           label = "The Rising Popularity of...",
-           x = as_datetime("2021-10-01"),  y = 95, 
-           family = "Helvetica Neue", fontface = "bold", size = 7.5, hjust = 0) +
-  annotate("text", x =  as_datetime("2021-10-01"), y = 80, 
-           label = str_wrap("The plot shows normalized Google Search hits for \"Wordle\"
+  geom_textbox(aes(x =  as_datetime("2021-10-01"), y = 65),
+               stat = "unique", 
+           label = "The plot shows normalized **Google Web Search** hits for \"Wordle\"
            in web searches worldwide from October 2021 to March 2022. A value of 100 denotes 
-           maximum search interest.", width = 50),
+           maximum search interest.",
+           width = unit(0.6, "npc"),
+           box.size = 0,  box.r = unit(0, "mm"), box.padding = unit(0, "mm"),
            family = "Noto Serif", size = 3, hjust = 0, color = "grey30") +
   scale_x_datetime(date_breaks = "1 months", date_labels = "%b") +
   scale_y_continuous(position = "right") +
@@ -58,3 +61,4 @@ df %>%
     axis.title.y = element_text(hjust = 0.2),
     axis.text = element_text(color = "grey50"))
 ggsave(here(base_path, "05-slope-wordle.png"), width = 6, height = 5)
+
