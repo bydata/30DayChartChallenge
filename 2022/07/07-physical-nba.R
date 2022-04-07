f6ef23d413d72f1b66e10c8348feccbd85fd4b77
bydata <ansgar.wolsing@gmail.com>
2022-04-07 12:56:10 +0200

    changed subtitle; moved labels

--- 2022/07/07-physical-nba.R
+++ 2022/07/07-physical-nba.R
@@ -57,6 +57,7 @@ theme_update(
   axis.ticks.x = element_line(color = text_color),
   axis.ticks.length.x = unit(2, "mm"),
   plot.title = element_text(color = "white", size = 14, family = "Poppins SemiBold"),
+  plot.subtitle = element_markdown(),
   plot.caption = element_textbox_simple(
     hjust = 0, margin = margin(t = 8, b = 2)
   )
@@ -67,7 +68,7 @@ p_height <- players %>%
   ggplot(aes(position, height)) +
   stat_halfeye(aes(fill = position), col = "white") +
   geom_text(aes(position, y = min(height), label = position, col = position), 
-            stat = "unique", size = 4, hjust = 0, nudge_x = 0.2, family = "Bangers") +
+            stat = "unique", size = 4, hjust = 0, nudge_x = 0.3, family = "Bangers") +
   paletteer::scale_color_paletteer_d("miscpalettes::jojo") +
   paletteer::scale_fill_paletteer_d("miscpalettes::jojo") +
   coord_flip() +
@@ -79,7 +80,7 @@ p_weight <- players %>%
   ggplot(aes(position, weight, fill = position, col = position)) +
   stat_halfeye(col = "white") +
   geom_text(aes(position, y = max(weight), label = position), stat = "unique",
-            hjust = 1, size = 4, nudge_x = 0.2, family = "Bangers") +
+            hjust = 1, size = 4, nudge_x = 0.3, family = "Bangers") +
   scale_y_continuous(breaks = seq(150, 350, 50)) +
   paletteer::scale_color_paletteer_d("miscpalettes::jojo") +
   paletteer::scale_fill_paletteer_d("miscpalettes::jojo") +
@@ -92,9 +93,10 @@ n_players <- nrow(players)
 p_height + p_weight +
   plot_annotation(
     title = "NBA Player Heights and Weights",
-    subtitle = glue::glue("{scales::number(n_players, big.mark = ',')} NBA professional from 1951 to 2019"),
+    subtitle = glue::glue("Height and weight attributes of
+                          {scales::number(n_players, big.mark = ',')}
+                          NBA professionals from 1951 to 2019"),
     caption = "Positions Guard-Forward and Forward-Guard as well as Forward-Center and Center-Forward 
-    merged.<br>Source: NBA, Kaggle. Visualization: Ansgar Wolsing"
-  )
+    merged.<br>Source: NBA, Kaggle. Visualization: Ansgar Wolsing")
 ggsave(here(base_path, "07-physical-nba-height-weight.png"), width = 7, height = 4)
 
