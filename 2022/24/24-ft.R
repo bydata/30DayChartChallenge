a4d65ae1f63eddf13a25cb0eb9efe167f5e2fe67
bydata <ansgar.wolsing@gmail.com>
2022-04-24 00:40:32 +0200

    back to Outfit

--- 2022/24/24-ft.R
+++ 2022/24/24-ft.R
@@ -48,37 +48,68 @@ covid_df_long <- prep_long_data(covid_cases, metric = "cases") %>%
   # filter(date2 >= start_date & date2 <= today() - duration("1 day"))
   filter(date2 >= start_date & date2 <= as_date("2022-03-11"))
 
+
+# Financial Times theme
+theme_ft <- function(...) {
+  theme_minimal(base_family = "Outfit Medium", base_size = 16) + 
+    theme(
+      plot.background = element_rect(color = NA, fill = "#FFF1E5"),
+      panel.background = element_rect(color = NA, fill = NA),
+      panel.grid = element_blank(),
+      panel.grid.major.y = element_line(color = "#E3DACE", size = 0.3),
+      text = element_text(color = "#68625D", lineheight = 1.3),
+      plot.title = element_textbox(color = "black", family = "Outfit Medium", 
+                                   face = "plain", size = 20, width = 1),
+      plot.title.position = "plot",
+      plot.subtitle = element_markdown(family = "Outfit Medium"),
+      plot.caption = element_markdown(
+        family = "Outfit Medium", hjust = 0, size = 11.5, color = "#5E5751"),
+      plot.caption.position = "plot",
+      axis.title = element_blank(),
+      axis.text.x = element_text(hjust = 0, color = "#68625D", size = 14),
+      axis.text.y.left = element_markdown(family = "Outfit Medium"),
+      axis.ticks.x = element_line(size = 0.3),
+      axis.ticks.length.x = unit(1.8, "mm"),                     
+      plot.margin = margin(t = 12, b = 2, l = 1, r = 1),
+      strip.text = element_blank()   # remove default facet titles
+    )
+}
+
+
+# Colors
 color_cases <- "#71C8E4"
 color_cases_text <- "#258BC3"
 color_deaths <- "#CE3240" 
+
+# Annotations ------------------------------------------------------------------
 
 plot_titles <- list(
   title = "Cases are translating into deaths at much higher rates in
   Hong Kong than in New Zealand, where elderly vaccination rates are much higher  ",
   subtitle = glue("Daily
-  <span style='color:{color_cases_text}; font-family: \"Test Metric SemiBold\"'>cases</span>
+  <span style='color:{color_cases_text}; font-family: \"Outfit SemiBold\"'>cases</span>
   per 100,000 people, and daily
-  <span style='color:{color_deaths}; font-family: \"Test Metric SemiBold\"'>deaths</span>
+  <span style='color:{color_deaths}; font-family: \"Outfit SemiBold\"'>deaths</span>
   per 2 million"),
   caption = "Cases shifted forward (14 days) to account for lag between infection and death.<br>
   Original plot by John Burn-Murdoch (Financial Times).
   Data: Johns Hopkins University, Our World in Data")
 
-country_annotations <- data.frame(region = c("Hong Kong", "New Zealand"),
-                                  label = c(
-                                    glue(
-                                      "<b style='color: black; font-size: 14pt'>Hong Kong</b><br>
+country_annotations <- data.frame(
+  region = c("Hong Kong", "New Zealand"),
+  label = c(
+    glue("<span style='color: black; font-size: 14pt; font-family: \"Outfit SemiBold\"'>
+    Hong Kong</span><br>
     66% of over-80s unvaccinated<br>when Omicron took off<br>
-    <span style='color: {color_deaths}; font-family: \"Test Metric SemiBold\"'>Case fatality
-         <br>rate: 4.7%</span>"
-                                    ),
-                                    glue(
-                                      "<b style='color: black; font-size: 14pt'>New Zealand</b><br>
+    <span style='color: {color_deaths}; font-family: \"Outfit SemiBold\"'>Case fatality
+         <br>rate: 4.7%</span>"),
+    glue("<span style='color: black; font-size: 14pt; font-family: \"Outfit SemiBold\"'>
+    New Zealand</span><br>
     2% unvaccinated<br>
-    <span style='color: {color_deaths}; font-family: \"Test Metric SemiBold\"'>CFR: 0.1%</span>"
-                                    )
-                                  ))
+    <span style='color: {color_deaths}; font-family: \"Outfit SemiBold\"'>CFR: 0.1%</span>")))
 
+
+# Create the plot and add the visual element -----------------------------------
 ragg::agg_png(
   here(base_path, "24-ft.png"), res = 300, width = 2800, height = 1616, units = "px"
 )
@@ -93,8 +124,8 @@ covid_df_long %>%
   # country annotation
   geom_richtext(data = country_annotations,
             aes(x = as_date("2022-02-01"), y = 107.5, label = label),
-            size = 4, label.size = 0, fill = NA, family = "Test Metric",
-            color = "#68625D", hjust = 0, vjust = 1, lineheight = 1
+            size = 4, label.size = 0, fill = NA, family = "Outfit Medium",
+            color = "#68625D", hjust = 0, vjust = 1, lineheight = 1.2
             ) +
   scale_x_date(breaks = as_date(c("2022-02-01", "2022-03-01")), 
                date_labels = "%b") +
@@ -105,33 +136,15 @@ covid_df_long %>%
     color <- ifelse(x > 0, color_cases_text, color_deaths)
     glue::glue("<span style='color: {color}'>{value}</span>")
     }) +
-  coord_cartesian(ylim = c(NA, 100), clip = "off") +
+  coord_cartesian(ylim = c(-60, 100), clip = "off") +
   facet_wrap(vars(region)) +
   labs(
     title = plot_titles$title,
     subtitle = plot_titles$subtitle,
     caption = plot_titles$caption
   ) +
-  theme_minimal(base_family = "Test Metric Medium", base_size = 16) + 
-  theme(
-    plot.background = element_rect(color = NA, fill = "#FFF1E5"),
-    panel.background = element_rect(color = NA, fill = NA),
-    panel.grid = element_blank(),
-    panel.grid.major.y = element_line(color = "#E3DACE", size = 0.3),
-    text = element_text(color = "#68625D", lineheight = 1.3),
-    plot.title = element_textbox(color = "black", family = "Test Metric Medium", 
-                                 face = "plain", size = 18, width = 1),
-    plot.title.position = "plot",
-    plot.subtitle = element_markdown(family = "Test Metric"),
-    plot.caption = element_markdown( family = "Test Metric Medium",
-      hjust = 0, size = 10, color = "#5E5751"),
-    axis.title = element_blank(),
-    axis.text.x = element_text(hjust = 0, color = "#68625D"),
-    axis.text.y.left = element_markdown(family = "Test Metric Medium"),
-    axis.ticks.x = element_line(size = 0.3),
-    axis.ticks.length.x = unit(1.8, "mm"),                     
-    plot.margin = margin(t = 12, b = 2, l = 1, r = 1)
-  )
+  theme_ft()
+  
 
  # Add black thin rectangle in the top left corner
 grid.rect(
@@ -140,5 +153,6 @@ grid.rect(
 )
 invisible(dev.off())
 
-# Test Metric font at https://klim.co.nz/test-fonts/
-# Alternative: https://www.ffonts.net/Cantarell-Regular.font.download
+# Raleway font at https://klim.co.nz/test-fonts/
+# Alternative: https://www.ffonts.net/Cantarell-Regular.font.download, Outfit
+
