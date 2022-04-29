c448c97fd810ee200a3009a0951e453bcfbc4d46
bydata <ansgar.wolsing@gmail.com>
2022-04-30 00:25:59 +0200

    day 29 - storytelling polls

---
+++ 2022/29/polls/29-storytelling-polls.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(lubridate)
+library(ggtext)
+library(here)
+library(glue)
+library(gganimate)
+
+base_path <- here("2022", "29", "polls")
+
+#' GETTING THE DATA
+#' Scrape the poll results on national level from Wahlrecht.de using the R script
+#' created by German weekly DIE ZEIT:
+#' https://github.com/ZeitOnline/wahltrend/blob/main/get_polls.R
+#' The script might need some adaptations to run on your local machine
+
+# Load the data scraped from Wahlrecht.de
+polls <- read_rds(here(base_path, "wahlrecht_umfragen.rds"))
+colnames(polls) <- c("id", "institute", "date", "respondents", "party", "share")
+polls <- polls %>% 
+  mutate(
+    party = case_when(
+      party == "CDUCSU" ~ "CDU/CSU",
+      party == "GRÜNE" ~ "GREENS",
+      party == "LINKE" ~ "LEFTIST",
+      TRUE ~party),
+    party = fct_inorder(party))
+
+
+# Custom theme
+theme_set(
+  theme_minimal(base_family = "Lato", base_size = 9) +
+    theme(legend.position = "bottom", 
+          legend.justification = "left", 
+          legend.key.height = unit(2.5, "mm"),
+          plot.background = element_rect(color = NA, fill = "grey98"),
+          panel.grid = element_blank(),
+          panel.grid.major.y = element_line(size = 0.3, color = "grey85"),
+          panel.grid.minor.y = element_line(size = 0.15, color = "grey85"),
+          text = element_text(color = "grey40", lineheight = 1.2),
+          plot.title = element_markdown(
+            color = "grey2", face = "bold"),
+          plot.title.position = "plot",
+          plot.subtitle = element_textbox_simple(
+            size = 8, hjust = 0, margin = margin(t = 2, b = 8), width = 0.95),
+          plot.caption = element_textbox_simple(
+            width = 1, size = 6, hjust = 0, 
+            margin = margin(t = 10, b = 4)),
+          axis.title = element_text(color = "grey45"),
+          axis.title.y = element_text(hjust = 1),
+          axis.title.y.right = element_blank(),
+          axis.text = element_text(color = "grey45"))
+)
+
+# Party colors
+unique(polls$party)
+party_colors <- c(
+  "CDU/CSU" = "grey9",
+  "SPD" = "#ca0002",
+  "GREENS" = rgb(100, 161, 45, maxColorValue = 255),
+  "FDP" = colorspace::darken("#ffed00", 0.1),
+  "LEFTIST" = "purple",
+  "AfD" = rgb(0, 158, 224, maxColorValue = 255))
+
+
+# Election date
+election_date <- as_date("2021-09-26")
+
+# Annotations
+annotations <- data.frame(
+  label = c("**1** | CDU/CSU surged<br>at the start of the<br>pandemic",
+            "**2** | Beginning of 2021, CDU/CSU lost support,<br>
+            while Greens and FDP gained in the polls"),
+  x = c(as_date("2019-11-01"), as_date("2020-11-01")),
+  y = c(38, 32),
+  hjust = 0)
+
+annotations$date_id <- seq_along(annotations$label)
+
+## All parties
+p <- polls %>%
+  filter(party %in% c("CDU/CSU", "SPD", "GREENS", "FDP")) %>% 
+  filter(date >= election_date - period("2 years") & date < election_date) %>% 
+  # group_by(date) %>% 
+  # mutate(date_id = cur_group_id()) %>% 
+  # ungroup() %>% 
+  ggplot(aes(date, share)) +
+  geom_point(aes(color = party), alpha = 0.2, size = 0.5, shape = 21) +
+  geom_smooth(aes(color = party, fill = stage(party, after_scale = alpha(color, 0.2))), 
+              size = 0.2, se = TRUE, method = "loess", span = 0.1, level = 0.95) +
+  geom_text(data = ~filter(., date == max(date)),
+            aes(y = share, label = party, color = party),
+            stat = "summary", fun = mean, nudge_x = 1, hjust = 0, vjust = 0.5, family = "Lato", 
+            fontface = "bold", size = 2.5
+            ) +
+  geom_richtext(
+    data = annotations,
+    aes(x, y, label = label, hjust = hjust),
+    inherit.aes = FALSE,
+    family = "Lato", size = 2.5, color = "grey99", fill = alpha("grey2", 0.6), 
+    label.r = unit(1, "mm"), label.size = 0.1, label.colour = "grey60"
+  ) +
+  # geom_segment() +
+  scale_x_date(expand = expansion(add = c(10, 45)), breaks = "3 months",
+               date_labels = "%b %Y") +
+  scale_y_continuous(breaks = seq(0, 50, 10), 
+                     labels = function(x) ifelse(x == 40, "40%", x),
+                     sec.axis = dup_axis()) +
+  scale_color_manual(values = party_colors) +
+  coord_cartesian(ylim = c(0, NA), clip = "off") +
+  guides(color = "none", fill = "none") +
+  labs(
+    title = "Polling Trends in the Run-up to the German Federal Election 2021",
+    subtitle = "Many polls from different institutes were published in the run-up to the election.
+    Instead of looking at each individual published polling result, it is more valuable to
+    observe general trends. The trend lines are fitted using LOWESS 
+    (**Lo**cally **We**ighted **S**catterplot **S**moother).
+    ",
+    caption = "LOWESS bandwidth = 0.2. 
+    For every single 
+    data point a local regression is calculated, taking into account only a certain
+    range of neighboring data points. A weighting function ensures that the 
+    importance of these points decreases with increasing distance.
+    The ribbons around the lines indicate the 95% confidence intervals of the 
+    point estimates. Individual poll results are shown as points.<br>
+    Source: Wahlrecht.de. Visualization: Ansgar Wolsing",
+    x = NULL,
+    y = "Projected vote share"
+  )
+
+p + 
+  transition_reveal(date_id) 
+
+ggsave(here(base_path, "29-all-parties.png"), width = 6, height = 5)  
+
