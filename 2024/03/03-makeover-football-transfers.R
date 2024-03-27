ad66df9b258c8a85ac3ac25916a5db80504c88aa
bydata <ansgar.wolsing@gmail.com>
2024-03-27 18:58:36 +0100

    Day 03 - Makeover

---
+++ 2024/03/03-makeover-football-transfers.R
@@ -0,0 +1,129 @@
+library(tidyverse)
+library(ggtext)
+library(here)
+library(packcircles)
+library(rvest)
+
+base_path <- here("2024", "03")
+
+# Scrape historical record transfers
+transfermarkt_url <- "https://www.transfermarkt.com/transfers/transferrekordehistorie/statistik?land_id=0&ausrichtung=&spielerposition_id=&altersklasse=&w_s=&plus=1"
+page <- read_html(transfermarkt_url)
+table <- html_node(page, css = "table.items") |> 
+  html_table()
+
+# Prepare the data
+transfers <- table |> 
+  select(rank = 1, name = 4, position = 5, age = 6, transfer_season = 7, 
+         transfer_date = 8, departure_club = 12, departure_league = 13,
+         arrival_club = 16, arrival_league = 17, transfer_fee_eur = 18) |> 
+  mutate(
+    transfer_fee_eur = str_remove(transfer_fee_eur, "€"),
+    transfer_fee_eur = case_when(
+        str_detect(transfer_fee_eur, "m") ~ as.numeric(str_remove(transfer_fee_eur, "m")) * 1e6,
+        str_detect(transfer_fee_eur, "k") ~ as.numeric(str_remove(transfer_fee_eur, "k")) * 1e3,
+      ),
+    transfer_date = lubridate::mdy(transfer_date)
+    ) |> 
+  filter(!is.na(rank), !is.na(transfer_fee_eur), transfer_date > as.Date("1984-01-01"))
+
+write_rds(transfers, here(base_path, "record-transfers.rds"))
+
+
+# ggplot2 theme
+colors <- c("#FBFAFC", "#FFFFFF")
+gradient_fill <- grid::linearGradient(colors, group = FALSE)
+theme_set(
+  theme_minimal(base_family = "Libre Franklin Light") +
+    theme(
+      plot.background = element_rect(color = NA, fill = gradient_fill),
+      text = element_text(color = "#090909"),
+      axis.text = element_text(family = "Source Code Pro"),
+      axis.line.x = element_line(linewidth = 0.33),
+      plot.title = element_textbox(
+        color = "grey8", width = 1,
+        family = "Libre Franklin Medium", hjust = 0, size = 16,
+        margin = margin(t = 4, b = 4)),
+      plot.title.position = "plot",
+      plot.subtitle = element_textbox(
+        hjust = 0, color = "grey35", lineheight = 1.25, width = 1,
+        margin = margin(b = 8)),
+      plot.caption = element_markdown(),
+      plot.margin = margin(rep(4, 4)),
+      legend.position = "top",
+      # panel.grid.major.x = element_line(color = "grey70", linewidth = 0.15),
+      # panel.grid.minor.x = element_line(color = "grey70", linewidth = 0.08),
+      # panel.grid.major.y = element_blank(),
+      # panel.grid.minor.y = element_blank(),
+      panel.grid = element_blank(),
+      strip.text = element_text(
+        family = "Libre Franklin Medium", size = 10, color = "grey35",
+        margin = margin(t = 4, b = 1))
+    )
+)
+
+
+# Longest reign es record transfer?
+transfers |> 
+  select(name, transfer_date, transfer_fee_eur) |> 
+  mutate(reign = lag(transfer_date, 1, default = Sys.Date()) - transfer_date) |> 
+  arrange(desc(reign))
+
+
+transfers |> 
+  mutate(name = fct_reorder(name, -transfer_fee_eur)) |> 
+  ggplot(aes(name, transfer_fee_eur)) + 
+  geom_col(
+    aes(fill = rank == 1), 
+    colour = "white", size = 0.3) +
+  geom_text(
+    aes(
+      label = scales::number(
+        transfer_fee_eur, scale_cut = scales::cut_short_scale(), accuracy = 1),
+      color = transfer_fee_eur > 18e6,
+      hjust = ifelse(transfer_fee_eur > 18e6, 1.1, -0.1)),
+    family = "Libre Franklin SemiBold", size = 2
+  ) +
+  # geom_richtext(
+  #   data = transfers_packing, 
+  #   aes(x, y, size = transfer_fee_eur, 
+  #       label = 
+  #         paste0(
+  #           ifelse(str_length(name) > 10, str_replace(name, "\\s", "<br>"), name), 
+  #           "<br>", "**",
+  #           scales::number(transfer_fee_eur, accuracy = 1, scale = 1e-6, suffix = "m"),
+  #           "**")), 
+  #   color = "white", family = "Outfit", fill = NA, label.size = 0) +
+  # annotate(
+  #   GeomTextBox,
+  #   x = 11400 + 4600, y = 9150 + 3200,
+  #   label = "Zidane held the record for the longest period (2001 to 2009)",
+  #   width = 0.25, family = "Outfit Light", size = 2, hjust = 0,
+  #   fill = NA, box.size = 0
+  # ) +
+  # annotate(
+  #   GeomCurve,
+  #   x = 11400 + 5150, xend = 11400 + 3300, y = 9150 + 3200, yend = 9150 + 2900,
+  #   curvature = 0.2, size = 0.25, color = "grey10",
+  #   arrow = arrow(length = unit(1.5, "mm"))
+  # ) + 
+  scale_y_continuous(
+    labels = scales::number_format(scale_cut = scales::cut_short_scale()),
+    breaks = seq(0, 200e6, 50e6),
+    expand = expansion(mult = c(0, 0.1))) +
+  scale_fill_manual(values = c("#1d00db", "#db00be")) +
+  scale_color_manual(values = c("grey2", "white")) +
+  coord_flip() +
+  labs(
+    title = "All record-high transfers in association football at their time
+    since Maradona to Napoli",
+    caption = "Transfer fees in EUR (at historical prices).<br>
+    Source: transfermarkt.de. Visualisation: Ansgar Wolsing"
+  ) +
+  # theme_void(base_family = "Outfit") + 
+  theme(
+    legend.position = "none",
+    axis.title.y = element_blank(),
+    axis.text.y = element_text(family = "Libre Franklin"))
+ggsave(here(base_path, "03-makeover-transfer-records.png"), width = 5, height = 5)
+
