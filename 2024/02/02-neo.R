148fe18c923102dd7dcd8f75843f1cd5102a0a46
bydata <ansgar.wolsing@gmail.com>
2024-04-01 14:36:37 +0200

    changed from US to worldwide gross earnings

--- 2024/02/02-neo.R
+++ 2024/02/02-neo.R
@@ -1,88 +1,67 @@
 library(tidyverse)
 library(ggtext)
-library(rvest)
 library(patchwork)
 library(here)
 
 base_path <- here("2024", "02")
 
 #' Source: Box Office Mojo (IMDb Pro)
-#' https://www.boxofficemojo.com/franchise/fr3142029061/
+df <- read_tsv(here(base_path, "matrix-box-office-gross.tsv"))
 
-# Scrape the date from the page
-url <- "https://www.boxofficemojo.com/franchise/fr3142029061/"
-page <- read_html(url)
-df_raw <- page |> 
-  html_node("table") |> 
-  html_table()
-
-# Prepare dataset
-df <- df_raw |> 
-  janitor::clean_names() |> 
+df_long <- df |> 
+  mutate(us_release_date = mdy(us_release_date)) |> 
+  pivot_longer(
+    cols = c(domestic_gross, international_gross),
+    names_to = "region", names_transform = function(x) str_remove(x, "_gross"),
+    values_to = "gross") |> 
   mutate(
-    across(everything(), function(x) na_if(x, "-")),
-    across(c(lifetime_gross, opening, max_theaters, open_th), function(x) as.numeric(str_remove_all(x, "[$,]"))),
-    release_date = mdy(release_date),
-    estimated = as.logical(estimated)
-    ) |> 
-  # select only the original movies, exclude re-releases
-  filter(release %in% c("The Matrix Reloaded", "The Matrix", "The Matrix Revolutions", 
-                        "The Matrix Resurrections"))
+    release = toupper(release),
+    release = fct_reorder(release, us_release_date),
+    region = factor(region, levels = c("international", "domestic"),
+                    labels = c("International", "US"))) |>
+  mutate(
+    gross_label_pos = lag(gross, 1, default = 0) + gross / 2,
+    .by = release) 
 
-df |> 
-  select(release, release_date, lifetime_gross, opening) |> 
-  deparse() |> 
-  writeLines()
-
-# Instead of scraping the data
-df <- structure(
-  list(
-    release = c("The Matrix Reloaded", "The Matrix", "The Matrix Revolutions", 
-                "The Matrix Resurrections"),
-    release_date = structure(c(12187,10681, 12361, 18983), class = "Date"), 
-    lifetime_gross = c(281576461, 171479930, 139313948, 37686805), 
-    opening = c(91774413, 27788331, 48475154, 10749011)), 
-  row.names = c(NA, -4L), 
-  class = c("tbl_df", "tbl", "data.frame"))
-        
 bar_width <- 0.5
 matrix_green <- colorspace::lighten("#75C2A0", 0.5)
 colorspace::lighten("#365989", 0.8)
 
-p <- df |> 
-  mutate(
-    # release = sprintf("%s (%d)", toupper(release), year(release_date)),
-    release = toupper(release),
-    release = fct_reorder(release, release_date)) |> 
-  arrange(release) |> 
-  ggplot(aes(y = fct_rev(release))) +
+p <- 
+  df_long |> #View()
+  ggplot(aes(y = release, group = region)) +
   ggfx::with_inner_glow(
     geom_col(
-    aes(x = lifetime_gross, fill = "Overall"),
+    aes(x = gross, fill = region),
     width = bar_width),
     sigma = 2, expand = 1, colour = matrix_green) +
-  ggfx::with_inner_glow(
-    geom_col(
-    aes(x = opening, fill = "Opening week"),
-    width = bar_width),
-    sigma = 2, expand = 1, colour = matrix_green) +
+  # Data labels inside bars
   geom_text(
-    aes(x = lifetime_gross, 
-        label = scales::number(lifetime_gross, scale_cut = scales::cut_long_scale())),
-    hjust = 0, nudge_x = 5e6, family = "Source Sans Pro SemiBold", color = matrix_green
+    aes(x = gross_label_pos, 
+        label = scales::number(gross, 
+                               accuracy = 1, scale_cut = scales::cut_long_scale())),
+    hjust = 0.5, family = "Source Sans Pro SemiBold", size = 3, color = "grey20"
   ) +
-  # Annotation inside the longest bar
+  # Data label for worldwide numbers
   geom_text(
-    data = ~subset(., lifetime_gross == max(lifetime_gross)),
-    aes(x = opening / 2, 
-        label = "Opening week"), 
-    color = "grey9", hjust = 0.5, family = "Source Sans Pro"
+    aes(x = worldwide_gross,
+        label = scales::number(worldwide_gross, accuracy = 1, scale_cut = scales::cut_long_scale())),
+    hjust = 0, nudge_x = 5e6, family = "Source Sans Pro SemiBold", size = 3,
+    color = matrix_green
   ) +
+  # Annotation for the regions inside the longest bar
+  # geom_text(
+  #   data = ~subset(., worldwide_gross == max(worldwide_gross)),
+  #   aes(x = gross_label_pos,
+  #       label = paste(region, "earnings")),
+  #   color = "grey40", hjust = 0.5, vjust = 2.3, size = 2.5, family = "Source Sans Pro"
+  # ) +
+  # Annotation for the regions inside the first bar
   geom_text(
-    data = ~subset(., lifetime_gross == max(lifetime_gross)),
-    aes(x = opening + (lifetime_gross - opening) / 2, 
-        label = "After opening week"), 
-        color = "grey9", hjust = 0.5, family = "Source Sans Pro"
+    data = ~subset(., us_release_date == min(us_release_date)),
+    aes(x = gross_label_pos,
+        label = paste(region, "earnings")),
+    color = "grey40", hjust = 0.5, vjust = 2.3, size = 2.5, family = "Source Sans Pro"
   ) +
   scale_y_discrete(expand = c(0.4, 0)) +
   scale_fill_manual(values = c("#EFFEFC", matrix_green)) +
@@ -91,7 +70,7 @@ p <- df |>
   guides(fill = "none") +
   labs(
     title = "Box office gross earnings of The Matrix films",
-    subtitle = "(in USD)",
+    subtitle = "(in million USD)",
     caption = "Source: Box Office Mojo (IMDb Pro). Visualization: Ansgar Wolsing") +
   theme_minimal(base_family = "Source Sans Pro") +
   theme(
@@ -115,13 +94,13 @@ p <- df |>
 p_annotations <- ggplot() +
   annotate(
     "text", x = 4.1, y = 3.15, label = c("Total"),
-    color = matrix_green,
+    color = matrix_green, size = 3,
     family = "Source Sans Pro", hjust = 0.8
   ) +
   annotate(
     "curve",
     x = 4.1, xend = 4.1, y = 3.05, yend = 2.7, 
-    curvature = -0.2, color = matrix_green, linewidth = 0.3,
+    curvature = -0.2, color = matrix_green, linewidth = 0.25,
     arrow = arrow(angle = 20, length = unit(1.3, "mm"))
   ) +
   coord_cartesian(xlim = c(0, 4), ylim = c(0, 4)) +
