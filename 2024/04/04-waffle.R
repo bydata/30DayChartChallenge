91987f709f0f29e7f9b6d3e933cbfac4f0b7c283
bydata <ansgar.wolsing@gmail.com>
2024-04-03 20:20:57 +0200

    cleanup

--- 2024/04/04-waffle.R
+++ 2024/04/04-waffle.R
@@ -14,8 +14,9 @@ theme_set(
     theme(
       plot.background = element_rect(color = NA, fill = gradient_fill),
       text = element_text(color = "#090909"),
-      axis.text = element_text(family = "Source Code Pro"),
-      axis.line.x = element_line(linewidth = 0.33),
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.line = element_blank(),
       plot.title = element_text(
         color = "grey8",
         family = "Libre Franklin SemiBold", hjust = 0, size = 16,
@@ -26,86 +27,22 @@ theme_set(
         margin = margin(b = 8)),
       plot.margin = margin(rep(4, 4)),
       legend.position = "top",
-      panel.grid.major.x = element_blank(),
-      panel.grid.minor.x = element_blank(),
-      panel.grid.major.y = element_line(color = "grey70", linewidth = 0.15),
+      panel.grid = element_blank(),
       # panel.grid.minor.y = element_line(color = "grey70", linewidth = 0.05)
       panel.grid.minor.y = element_blank(),
       strip.text = element_text(
-        family = "Libre Franklin SemiBold", size = 10, color = "grey35",
+        family = "Libre Franklin SemiBold", size = 10, color = "#3B1448",
         margin = margin(t = 4, b = 1))
     )
 )
+
 
 # Source: https://www.federalreserve.gov/releases/z1/dataviz/dfa/index.html
 df <- read_csv(here(base_path, "dfa-networth-shares.csv"),
                name_repair = janitor::make_clean_names)
 
-df |> 
-  filter(date == max(date)) |> 
-  summarize(sum(net_worth))
 
-df |> 
-  # select the latest data
-  filter(date == max(date)) |> 
-  select(date, category, net_worth) |>
-  mutate(
-    category = fct_inorder(category),
-    net_worth_rounded = round(net_worth)) |> 
-  # summarize(sum(net_worth_rounded))
-  ggplot(aes(fill = category, values = net_worth_rounded)) +
-  geom_waffle(na.rm = FALSE, make_proportional = TRUE) +
-  # scale_fill_manual(values = pal_office) +
-  coord_equal() +
-  labs()
-
-df |> 
-  # select the latest data
-  filter(date == max(date)) |> 
-  select(date, category, net_worth) |>
-  mutate(
-    category = fct_inorder(category),
-    category = factor(
-      category, 
-      labels = c("Top 0.1%", "Remaining Top 1%", "Next 9%", "Next 40%", "Bottom 50%")), 
-    net_worth_rounded = round(net_worth),
-    net_worth_rounded_remaining = 100 - net_worth_rounded) |> 
-  pivot_longer(cols = c(net_worth_rounded, net_worth_rounded_remaining),
-               names_to = "group", values_to = "value") |> 
-  # View()
-  ggplot(aes(fill = group, values = value)) +
-  geom_waffle(na.rm = FALSE, make_proportional = TRUE, color = "white", size = 0.3) +
-  scale_fill_manual(values = c("#3B1448", "grey80")) +
-  coord_equal() +
-  facet_wrap(vars(category), labeller = as_labeller(toupper)) +
-  labs()
-
-
-
-df_plot <- df |> 
-  # select the latest data
-  filter(date == max(date)) |> 
-  select(date, category, net_worth) |>
-  mutate(
-    category = fct_inorder(category),
-    category = factor(
-      category, 
-      labels = c("Top 0.1%", "Remaining Top 1%", "Next 9%", "Next 40%", "Bottom 50%")), 
-    net_worth_rounded = round(net_worth),
-    net_worth_rounded_cumul = cumsum(net_worth_rounded),
-    net_worth_rounded_cumul_lag = lag(net_worth_rounded_cumul, default = 0),
-    net_worth_rounded_remaining = 100 - net_worth_rounded_cumul_lag - net_worth_rounded,
-    net_worth_rounded_remaining = ifelse(net_worth_rounded_remaining < 0, 0, net_worth_rounded_remaining)) |> 
-  select(category, net_worth_rounded_cumul_lag, net_worth_rounded, net_worth_rounded_remaining) |> 
-  pivot_longer(cols = c(net_worth_rounded_cumul_lag, net_worth_rounded, net_worth_rounded_remaining),
-               names_to = "group", values_to = "value") |> 
-  mutate(group = factor(
-    group, 
-    levels = c("net_worth_rounded_cumul_lag", "net_worth_rounded", "net_worth_rounded_remaining"),
-    labels = c("previous", "this", "remaining")))
-
-
-# Without rounding
+# Prepare the data for the plot with shifting intervals
 df_plot <- df |> 
   # select the latest data
   filter(date == max(date)) |> 
@@ -140,12 +77,5 @@ df_plot |>
     title = "The top 0.1% have 5 times more total wealth than the bottom 50%",
     subtitle = "Net worth distribution in the U.S. (2023 Q3)",
     caption = "Source: Federal Reserve. Visualization: Ansgar Wolsing"
-  ) +
-  theme(
-    axis.title = element_blank(),
-    axis.text = element_blank(),
-    axis.line.x = element_blank(),
-    strip.text = element_text(color = "#3B1448"),
-    panel.grid.major.y = element_blank()
   )
 ggsave(here(base_path, "04-waffle.png"), width = 6, height = 4, scale = 1.5)
